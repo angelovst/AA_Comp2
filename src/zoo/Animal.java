3f29cb31ad3fec9f0478dcbc407160cb046f8fcb
Angelo <angelovst@hotmail.com>
2019-05-25 11:40:20 -0300

    Commit inicial

---
+++ src/zoo/Animal.java
@@ -0,0 +1,13 @@
+package zoo;
+
+public interface Animal {
+    void locomover();
+
+    Sexo getSexo();
+
+    void setSexo(Sexo sexo);
+
+    Porte getPorte();
+
+    void setPorte(Porte porte);
+}
---
+++ src/zoo/Porte.java
@@ -0,0 +1,5 @@
+package zoo;
+
+public enum Porte {
+    PEQUENO, MEDIO, GRANDE
+}
