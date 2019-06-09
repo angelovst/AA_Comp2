347c327490d028f0dda56580d338257819cdc4b2
angelovst <angelovst@hotmail.com>
2019-06-08 23:13:59 -0300

    Testes com animais

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import Animais.*;
+import Animais.Interfaces.Animal;
 import Enums.Alimento;
 import Enums.Localizacao;
 import Enums.Sexo;
@@ -6,11 +7,58 @@ import Instalacao.Animais.FabricaInstalacao;
 import Instalacao.Instalacao;
 import Zoo.Zoologico;
 
+import java.util.ArrayList;
+
 public class Main {
+
+    public static ArrayList<Animal> criaAnimais(){
+        Elefante elefante = new Elefante(Sexo.MASCULINO);
+        Baleia baleia = new Baleia(Sexo.FEMININO);
+        Foca foca = new Foca(Sexo.FEMININO);
+        Leao leao = new Leao(Sexo.MASCULINO);
+        Macaco macaco = new Macaco(Sexo.MASCULINO);
+        PeixeBoi peixeBoi = new PeixeBoi(Sexo.MASCULINO);
+        Pinguim pinguim = new Pinguim(Sexo.FEMININO);
+        Tartaruga tartaruga = new Tartaruga(Sexo.FEMININO);
+        Tubarao tubarao = new Tubarao(Sexo.MASCULINO);
+        Zebra zebra = new Zebra(Sexo.FEMININO);
+
+        ArrayList<Animal> animais = new ArrayList<>();
+        animais.add(elefante);
+        animais.add(baleia);
+        animais.add(foca);
+        animais.add(leao);
+        animais.add(macaco);
+        animais.add(peixeBoi);
+        animais.add(pinguim);
+        animais.add(tartaruga);
+        animais.add(tubarao);
+        animais.add(zebra);
+
+        return animais;
+    }
+    public static void TestaAnimais(){
+        ArrayList<Animal> animais = criaAnimais();
+        String acao;
+
+        for(Animal animal : animais){
+            for(Alimento alimento : Alimento.values()){
+                if (animal.Alimentar(alimento)) {
+                    acao = " comeu ";
+                }
+                else{
+                    acao = " não come ";
+                }
+                System.out.println(animal.getClass().getSimpleName() + acao + alimento);
+            }
+            System.out.println(animal.getClass().getSimpleName() + " diz:");
+            animal.locomover();
+            System.out.println();
+        }
+    }
     public static void main(String[] args)
     {
         System.out.println("teste");
-        Zoologico zoo = new Zoologico();
 
         Elefante elef = new Elefante(Sexo.FEMININO);
         Elefante elef2 = new Elefante(Sexo.FEMININO);
@@ -23,12 +71,17 @@ public class Main {
                 comeu = " comeu ";
             }
             else{
-                comeu = " não comeu ";
+                comeu = " não come ";
             }
             System.out.println(elef.getClass().getSimpleName() + comeu + comida);
         }
 
-        zoo.adicionarInstalacao(FabricaInstalacao.criaInstalacaoElefante(Localizacao.CENTROESTE));
+        Zoologico zoo = new Zoologico();
+
+        boolean foi = zoo.adicionarInstalacao(FabricaInstalacao.criaInstalacaoElefante(Localizacao.CENTROESTE));
+        System.out.print(foi);
+        foi = zoo.adicionarInstalacao(FabricaInstalacao.criaInstalacaoElefante(Localizacao.CENTROESTE));
+        System.out.print(foi);
 
         Instalacao ints = FabricaInstalacao.criaInstalacaoElefante(Localizacao.CENTROESTE);
 
@@ -41,6 +94,8 @@ public class Main {
         if(ints.adicionarAnimais(elef3)){
             System.out.println(elef.getClass().getSimpleName() + "add");
         }
+
+        TestaAnimais();
 
     }
 }
